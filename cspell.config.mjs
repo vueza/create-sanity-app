ec4fd786a59b28fdb84da149cd1d744342f876c4
Tobey Codes <tobey@tobey.codes>
2025-03-08 15:32:32 -0600

    feat: add CODEOWNERS file

--- cspell.config.mjs
+++ cspell.config.mjs
@@ -1,5 +1,5 @@
 /** @type { import("@cspell/cspell-types").CSpellUserSettings } */
 export default {
   ignorePaths: ["node_modules/**", "package.json", "pnpm-lock.yaml"],
-  words: ["cspell", "commitlint", "syncpack"],
+  words: ["cspell", "commitlint", "syncpack", "codeowners"],
 };
