ef4f24a6f73abb42235f7b161e17a194ecfa5e43
Tobey Codes <tobey@tobey.codes>
2025-03-08 15:37:25 -0600

    feat: add turborepo

--- cspell.config.mjs
+++ cspell.config.mjs
@@ -1,5 +1,5 @@
 /** @type { import("@cspell/cspell-types").CSpellUserSettings } */
 export default {
   ignorePaths: ["node_modules/**", "package.json", "pnpm-lock.yaml"],
-  words: ["cspell", "commitlint", "syncpack", "codeowners"],
+  words: ["cspell", "commitlint", "syncpack", "codeowners", "turborepo"],
 };
