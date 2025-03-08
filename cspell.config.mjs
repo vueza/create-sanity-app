f230fd1539cc253c84a572b68f3708d40294ca82
Tobey Codes <tobey@tobey.codes>
2025-03-08 15:16:23 -0600

    feat: add cspell

---
+++ cspell.config.mjs
@@ -0,0 +1,5 @@
+/** @type { import("@cspell/cspell-types").CSpellUserSettings } */
+export default {
+  ignorePaths: ["node_modules/**", "package.json", "pnpm-lock.yaml"],
+  words: ["cspell", "commitlint", "syncpack"],
+};
