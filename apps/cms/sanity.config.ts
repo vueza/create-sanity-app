260315bd166ad68c178dd6e6978d0470f14a9a42
Tobey Codes <tobey@tobey.codes>
2025-03-11 20:58:29 -0600

    feat: add sanity media plugin

--- apps/cms/sanity.config.ts
+++ apps/cms/sanity.config.ts
@@ -1,6 +1,7 @@
 import { assist } from "@sanity/assist";
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
+import { media } from "sanity-plugin-media";
 import { presentationTool } from "sanity/presentation";
 import { structureTool } from "sanity/structure";
 import { document } from "./src/document";
@@ -23,6 +24,7 @@ export default defineConfig({
     structureTool(structure),
     assist(),
     visionTool(),
+    media(),
   ],
   schema: {
     types,
