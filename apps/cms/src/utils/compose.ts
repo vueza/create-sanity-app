f1e97d359e5e450b54d6e6a100779af7ba339801
Tobey Codes <tobey@tobey.codes>
2025-03-16 21:32:28 -0600

    refactor: compose function to improve readability

--- apps/cms/src/utils/compose.ts
+++ apps/cms/src/utils/compose.ts
@@ -1,6 +1,9 @@
 import type { DocumentDefinition } from "sanity";
 
-export const compose =
-  (...rest: ((arg: DocumentDefinition) => DocumentDefinition)[]) =>
-  (x: DocumentDefinition) =>
-    rest.reduceRight((y, f) => f(y), x);
+export const compose = (
+  ...functions: ((document: DocumentDefinition) => DocumentDefinition)[]
+) => {
+  return (document: DocumentDefinition): DocumentDefinition => {
+    return functions.reduceRight((result, fn) => fn(result), document);
+  };
+};
