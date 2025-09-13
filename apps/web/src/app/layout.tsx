f88b1cc3ebcad0207ea6f996d01562e16d00cb1a
Tobey Codes <tobey@tobey.codes>
2025-09-13 16:27:54 -0600

    fix: sanity imports

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { draftMode } from "next/headers";
-import { VisualEditing } from "next-sanity";
+import { VisualEditing } from "next-sanity/visual-editing";
 import type { ReactNode } from "react";
 import { DraftModeButton } from "../components/draft-mode-button";
 import "@company/config-tailwind/app.css";
